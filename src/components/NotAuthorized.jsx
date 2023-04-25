560154ce83c273ac7c76aad728ea40caae1a578c
rabin9840 <rabinshrestha984156@gmail.com>
2023-04-25 11:23:05 +0545

    created not authorized component

--- src/components/NotAuthorized.jsx
+++ src/components/NotAuthorized.jsx
@@ -4,9 +4,11 @@ const NotAuthorized=()=>{
         <div className="flex flex-col items-center justify-center h-center">
             <h1 className="text-3xl font-bold mb-4">Not Authorized</h1>
             <p className="text-lg mb-4">You are not authorized to access this page.</p>
-            
+
         </div>
             
         </>
     )
 }
+
+export default NotAuthorized;
